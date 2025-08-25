47f2126361402945a7ba820caa261aa52402c759
Antoine Emerit <antoine.emerit@online.fr>
2025-08-25 04:55:53 +0200

    Re-enabled skipped extensions, functions, and types thanks to previous fixes

--- scripts/data.py
+++ scripts/data.py
@@ -5,15 +5,6 @@
 """Collections of strings and names that affect the projection generation."""
 
 EXCLUDED_EXTENSIONS = (
-    # Atom not projecting right?
-    "XR_MSFT_controller_model",
-    # Projection of static string fails
-    "XR_MSFT_spatial_graph_bridge",
-    "XR_MSFT_spatial_anchor_persistence",
-    "XR_MSFT_holographic_window_attachment",
-    # Projection of UuidMSFT fails
-    "XR_MSFT_scene_understanding",
-    "XR_MSFT_scene_understanding_serialization",
 )
 
 
@@ -26,8 +17,6 @@ VALID_FOR_NULL_INSTANCE = set((
 
 # These break the projection right now.
 SKIP = set((
-    'xrGetSceneComputeStateMSFT',
-    'xrGetSwapchainStateFB',
 ))
 
 DISCOURAGED = set((
@@ -51,8 +40,6 @@ MANUALLY_PROJECTED = set((
 SKIP_PROJECTION = set((
     "XrBaseInStructure",
     "XrBaseOutStructure",
-    # Array of XrColor4f not getting initialized right
-    "XrPassthroughColorMapMonoToRgbaFB",
 ))
 
 UPPER_TOKENS = set((
